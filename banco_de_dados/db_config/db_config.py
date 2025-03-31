d65a607037b7a803a3968bd14d419dc5a6577037
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2025-03-31 16:51:50 -0300

    fix: alterar banco de dados

--- banco_de_dados/db_config/db_config.py
+++ banco_de_dados/db_config/db_config.py
@@ -1,7 +1,7 @@
 # Configuração do banco de dados PostgreSQL
 db_config = {
     "host": "localhost",    # Endereço do servidor do banco de dados
-    "database": "intuitiveCare",  # Nome do banco de dados
+    "database": "intuitive",  # Nome do banco de dados
     "user": "postgres",      # Nome de usuário para autenticação
     "password": "root"       # Senha do banco de dados
 }
