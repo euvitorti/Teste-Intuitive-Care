7a0a76b064a29aa8c6cf96e13c68db99cea5b8ef
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2025-04-07 15:05:23 -0300

    Reestrutura API para melhor organização e adiciona testes unitários com pytest

---
+++ api/src/service/api_service.py
@@ -0,0 +1,68 @@
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from banco_de_dados.db_config.db_config import db_config
+import os
+
+# Obtém o diretório base do arquivo atual
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Define o caminho do arquivo SQL
+SQL_BUSCAR_OPERADORAS = os.path.join(BASE_DIR, "..", "sql", "buscar_operadoras.sql")
+
+def conectar_banco_de_dados():
+    """
+    Cria uma conexão com o banco de dados.
+    """
+    try:
+        return psycopg2.connect(**db_config)
+    except psycopg2.Error as e:
+        raise Exception(f"Erro ao conectar ao banco: {str(e)}")
+
+def carregar_query(caminho_sql: str) -> str:
+    """
+    Carrega e retorna o conteúdo de um arquivo SQL.
+
+    :param caminho_sql: Caminho para o arquivo SQL.
+    :return: String contendo a query SQL.
+    :raises Exception: Se o arquivo não for encontrado.
+    """
+    
+    try:
+        with open(caminho_sql, "r", encoding="utf8") as file:
+            return file.read()
+    except FileNotFoundError:
+        raise Exception("Arquivo SQL não encontrado.")
+
+def buscar_operadoras(termo: str):
+    """
+    Busca operadoras de saúde pela razão social no banco de dados.
+
+    :param termo: Termo de busca para filtrar as operadoras.
+    :return: Lista de operadoras encontradas ou None se não houver resultados.
+    :raises Exception: Se houver erro ao conectar ou executar a consulta.
+    """
+
+    try:
+        # Conecta ao banco de dados
+        conn = conectar_banco_de_dados()
+        cursor = conn.cursor(cursor_factory=RealDictCursor)
+
+        # Carrega a query SQL do arquivo
+        query = carregar_query(SQL_BUSCAR_OPERADORAS)
+
+        # Executa a consulta com o termo de busca, prevenindo SQL Injection
+        cursor.execute(query, (f"%{termo}%",))
+        resultados = cursor.fetchall()
+
+        # Retorna os resultados ou None se a lista estiver vazia
+        return resultados if resultados else None
+
+    except psycopg2.Error as e:
+        raise Exception(f"Erro ao buscar operadoras: {str(e)}")
+
+    finally:
+        # Fecha o cursor e a conexão apenas se estiverem abertos
+        if cursor is not None:
+            cursor.close()
+        if conn is not None:
+            conn.close()
---
+++ api/src/sql/buscar_operadoras.sql
@@ -0,0 +1,13 @@
+SELECT 
+    razao_social, 
+    nome_fantasia,
+    cnpj, 
+    modalidade, 
+    cidade, 
+    uf, 
+    telefone, 
+    endereco_eletronico, 
+    data_registro_ans
+FROM operadoras_ativas
+WHERE razao_social ILIKE %s
+LIMIT 10;
